09201ac9a3689dbd07e1d65d2d08de06dc6f77d9
LIU Hao <lh_mouse@126.com>
2025-04-29 16:34:58 +0800

    Don't install symlinks in /usr/local

--- makedeb.sh
+++ makedeb.sh
@@ -9,12 +9,12 @@ mkdir -p ${_tempdir}
 cp -pr DEBIAN -t ${_tempdir}
 
 mkdir -p ${_tempdir}/usr/include
-mkdir -p ${_tempdir}/usr/local/include
-
 ln -s libbson-1.0/bson ${_tempdir}/usr/include/bson
-ln -s libbson-1.0/bson ${_tempdir}/usr/local/include/bson
 ln -s libmongoc-1.0/mongoc ${_tempdir}/usr/include/mongoc
-ln -s libmongoc-1.0/mongoc ${_tempdir}/usr/local/include/mongoc
+
+#mkdir -p ${_tempdir}/usr/local/include
+#ln -s libbson-1.0/bson ${_tempdir}/usr/local/include/bson
+#ln -s libmongoc-1.0/mongoc ${_tempdir}/usr/local/include/mongoc
 
 sed -i "s/{_pkgname}/${_pkgname}/" ${_tempdir}/DEBIAN/control
 sed -i "s/{_pkgversion}/${_pkgversion}/" ${_tempdir}/DEBIAN/control
